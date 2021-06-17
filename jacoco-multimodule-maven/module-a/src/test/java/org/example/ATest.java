cbcddc3e8ee139bfe7dc2c7e148adda6865830af
StuAalto <68701478+StuAalto@users.noreply.github.com>
2021-06-17 09:23:21 +0200

    Update ATest.java
    Modification du contenu du test avec srcipt repris du ProjetBDD_Orchestrator

--- jacoco-multimodule-maven/module-a/src/test/java/org/example/ATest.java
+++ jacoco-multimodule-maven/module-a/src/test/java/org/example/ATest.java
@@ -1,12 +1,35 @@
 package org.example;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-public class ATest {
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 
-    @Test
-    public void a() throws Exception {
-        new A().foo();
+public class IsItFriday {
+    private String today;
+    private String actualAnswer;
+
+    private static String isItFriday(String today) {
+        if (today != "Friday"){
+            return "Nope";
+        }
+        return null;
     }
 
+    @Given("^today is Sunday$")
+    public void today_is_Sunday()  {
+        today = "Sunday";
+    }
+
+    @When("^I ask whether it's Friday yet$")
+    public void i_ask_whether_it_s_Friday_yet()  {
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("^I should be told \"([^\"]*)\"$")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+       System.out.println("FRIDAYYY");
+    }
 }
