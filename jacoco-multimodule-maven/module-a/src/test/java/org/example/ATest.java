8e109af5c62b5d520a86eed4530aa1319f114cab
StuAalto <68701478+StuAalto@users.noreply.github.com>
2021-06-17 09:53:19 +0200

    Update ATest.java
    Retour au script d'origine.

--- jacoco-multimodule-maven/module-a/src/test/java/org/example/ATest.java
+++ jacoco-multimodule-maven/module-a/src/test/java/org/example/ATest.java
@@ -1,35 +1,12 @@
 package org.example;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.When;
+public class ATest {
 
-public class IsItFriday {
-    private String today;
-    private String actualAnswer;
-
-    private static String isItFriday(String today) {
-        if (today != "Friday"){
-            return "Nope";
-        }
-        return null;
+    @Test
+    public void a() throws Exception {
+        new A().foo();
     }
 
-    @Given("^today is Sunday$")
-    public void today_is_Sunday()  {
-        today = "Sunday";
-    }
-
-    @When("^I ask whether it's Friday yet$")
-    public void i_ask_whether_it_s_Friday_yet()  {
-        actualAnswer = IsItFriday.isItFriday(today);
-    }
-
-    @Then("^I should be told \"([^\"]*)\"$")
-    public void i_should_be_told(String expectedAnswer) {
-        assertEquals(expectedAnswer, actualAnswer);
-       System.out.println("FRIDAYYY");
-    }
 }
